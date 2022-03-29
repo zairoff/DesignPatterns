5ddae29d4939dc60b389ad7d6162865d2b28f0ef
Maruf <marufzoiroff@gmail.com>
2022-03-29 14:28:38 +0500

    added imageViewer class to apply filters

---
+++ Adapter/ImageViewer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter
+{
+    public class ImageViewer
+    {
+        private readonly Image _image;
+
+        public ImageViewer(Image image)
+        {
+            _image = image;
+        }
+
+        public void ApplyFilter(IFilter filter)
+        {
+            filter.Apply(_image);
+        }
+    }
+}
