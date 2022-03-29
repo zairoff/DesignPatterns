6c4a2ce887ddc6ea9f060f0c0938c743e6037009
Maruf <marufzoiroff@gmail.com>
2022-03-29 16:01:01 +0500

    added source project to demonstration

---
+++ Factory/FiltersFactory.cs
@@ -0,0 +1,18 @@
+﻿using Adapter;
+using System;
+
+namespace Factory
+{
+    public class FiltersFactory<T>
+    {
+        public static IFilter<T> GetFilter(string filterType)
+        {
+            IFilter<T> filter = null;
+            switch (filterType)
+            {
+                case "blur": filter = new Blur(); break;
+                default: break;
+            }
+        }
+    }
+}
